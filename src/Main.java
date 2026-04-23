404806db196ba8330ec64cdbb9b6e6993fbde58d
Emilio Scott <milii76@outlook.com>
2026-04-22 20:34:27 -0700

    add beginning CLI for main.java

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,45 @@
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Rock Paper Scissors");
         System.out.println("How big would you like your map?");
-        try { 
+
+        //retieving rows and columns from user.
+        boolean columnsFlag = false;
+        boolean rowsFlag = false;
+
+        int columns= 0; 
+        int rows = 0;
+
+        while (!columnsFlag) {
             System.out.print("Please enter how many columns: ");
-            String input = scanner.nextLine().strip();
-            int columns = Integer.parseInt(input);
-        } catch (NumberFormatException e){
-            boolean integerFlag = false;
+            String input = scanner.nextLine().trim();
 
-            System.out.println("That is not a valid integer.");
-            System.out.print("please enter how many columns as an integer: ");
-            String input = scanner.nextLine()strip();
+            try {
+                columns = Integer.parseInt(input);
+                columnsFlag = true;
+            } catch (NumberFormatException e) {
+                System.out.println("That is not a valid integer.");
+            }
         }
-        
-       
-        
 
+        while(!rowsFlag){
+            System.out.print("How many rows?: ");
+            String input2 = scanner.nextLine().trim();
 
-        System.out.print("How many rows?: ");
-        String rows = scanner.nextLine();
+            try {
+                rows = Integer.parseInt(input2);
+                rowsFlag = true;
+            } catch (NumberFormatException e) {
+                System.out.println("that is not a valid integer.");
+            }
+        }
+
+        //beginning game! 
         for (int i= 0;  i < 3;  i++){
             int timer= 3-i;
             if (timer == 3){
@@ -35,13 +51,8 @@ public class Main {
             TimeUnit.SECONDS.sleep(1);
         }
 
-        World worldMap = new World(;
-        
-
-
-
-
+        //NEW WORLD
+        World world1 = new World(rows, columns);
 
     }
-
 }
