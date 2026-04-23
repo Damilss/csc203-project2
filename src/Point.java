5bd41c2012e4823b1e12da5813826f0e30d1f460
Emilio Scott <milii76@outlook.com>
2026-04-22 19:14:36 -0700

    docs teachers note in point.java

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,6 @@
 //The class point should be able to hold the positiion of each entity in the world
+//In other words, this it the active pointer, similar to a program counter, where this is used to manage the points
+//Teacher states that this class is kind of not needed and only required by the assignment for extra practice
 public class Point {
     Point(){
         
